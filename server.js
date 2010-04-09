9576942e46770ee09a35bb15540c8dc77c5b51b1
Tim Caswell <tim@creationix.com>
2010-04-09 18:21:14 -0500

    Update the style.

--- server.js
+++ server.js
@@ -3,4 +3,9 @@
 // You will need to install it from http://github.com/creationix/wheat
 
 var Wheat = require('wheat');
-Wheat(__dirname, 8080);
+var sys = require('sys');
+var path = __dirname;
+var port = 8080;
+var host = "127.0.0.1";
+sys.error("Starting wheat server using git repo at " + path);
+Wheat(path, port, host);
