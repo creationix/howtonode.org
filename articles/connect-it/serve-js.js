980beff4f85fb688ab602d482239bd597e2ac9c2
Tim Caswell <tim@creationix.com>
2010-06-08 12:08:46 -0700

    Put a warning in the serve-js example that it's not secure at all.

--- articles/connect-it/serve-js.js
+++ articles/connect-it/serve-js.js
@@ -1,6 +1,8 @@
 var fs = require('fs');
 
 exports.handle = function handle(err, req, res, next) {
+  // Serve any file relative to the process.
+  // NOTE security was sacrificed in this example to make the code simple.
   fs.readFile(req.url.substr(1), function (err, data) {
     if (err) {
       next(err);
