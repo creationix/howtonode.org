b1994d46d1a1b71e7d71b39aa6af6c6f75de32d6
Tim Caswell <tim@creationix.com>
2010-09-30 00:01:20 -0700

    Remove half published article.

--- articles/object-graphs/variables.js
+++
@@ -1,7 +0,0 @@
-// Let's create some local variables in the top scope
-var name = "Tim Caswell";
-var age = 28;
-var isProgrammer = true;
-var likesJavaScript = true;
-// Test to see if the two variables reference the same value
-isProgrammer === likesJavaScript;
