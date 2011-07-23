5a61c2901b93a03772dddab151361286483723eb
Larry Olson <loarabia@hotmail.com>
2011-07-22 20:50:38 -0700

    Updated mongo article provider.

--- articles/express-mongodb/articleprovider-mongodb.js
+++ articles/express-mongodb/articleprovider-mongodb.js
@@ -1,6 +1,8 @@
-var Db= require('mongodb/db').Db,
-    ObjectID= require('mongodb/bson/bson').ObjectID,
-    Server= require('mongodb/connection').Server;
+var Db = require('mongodb').Db;
+var Connection = require('mongodb').Connection;
+var Server = require('mongodb').Server;
+var BSON = require('mongodb').BSON;
+var ObjectID = require('mongodb').ObjectID;
 
 ArticleProvider = function(host, port) {
   this.db= new Db('node-mongo-blog', new Server(host, port, {auto_reconnect: true}, {}));
