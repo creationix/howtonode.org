11fd21942e606fbf64d3b71793e476fd615ac46d
Larry Olson <loarabia@hotmail.com>
2011-07-24 18:35:00 -0700

    blog_index.jade is now index.jade

--- articles/express-mongodb/app.js
+++ articles/express-mongodb/app.js
@@ -33,7 +33,7 @@ var articleProvider = new ArticleProvider('localhost', 27017);
 
 app.get('/', function(req, res){
     articleProvider.findAll( function(error,docs){
-        res.render('blog_index.jade', { 
+        res.render('index.jade', { 
             locals: {
                 title: 'Blog',
                 articles:docs
