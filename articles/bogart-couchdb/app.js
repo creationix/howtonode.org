a1a9b23216e3268bf7fff5ad1812b4c2b810cf8f
nrstott <nrstott@gmail.com>
2011-12-14 17:11:35 -0600

    updead bogart-couchdb article to reflect changes to bogart

--- articles/bogart-couchdb/app.js
+++ articles/bogart-couchdb/app.js
@@ -1,66 +1,77 @@
-var bogart  = require('bogart');
-var couchdb = require('couchdb');
+var bogart  = require('bogart')
+  , couchdb = require('couchdb')
+  , settings = require('./settings');
 
-var app = bogart.router(function(get, post, put, destroy) {
+var client     = couchdb.createClient(settings.port, settings.host, { user: settings.user, password: settings.password })
+  , db         = client.db(settings.db)
+  , viewEngine = bogart.viewEngine('mustache')
+  , router     = bogart.router();
 
-  var client     = couchdb.createClient(5984, '127.0.0.1', { user: 'nathan', password: 's4stott' })
-    , db         = client.db('blog')
-    , viewEngine = bogart.viewEngine('mustache');
+router.get('/', function(req) {
+  return bogart.redirect('/posts');
+});
 
-  get('/posts', function(req) {
+router.get('/posts', function(req) {
     
-    return db.view('blog', 'posts_by_date').then(function(resp) {
-      var posts = resp.rows.map(function(x) { return x.value; });
-      
-      return viewEngine.respond('posts.html', {
-        locals: {
-          posts: posts,
-          title: 'Blog Home'
-        }
-      });
-    });
-  });
-
-  get('/posts/new', function(req) {
-    return viewEngine.respond('new-post.html', {
+  return db.view('blog', 'posts_by_date').then(function(resp) {
+    var posts = resp.rows.map(function(x) { return x.value; });
+    
+    return viewEngine.respond('posts.html', {
       locals: {
-        title: 'New Post'
+        posts: posts,
+        title: 'Blog Home'
       }
     });
+  }, function(err) {
+    if (err.error && err.error === 'not_found') {
+      return bogart.error('execute syncDesignDoc before trying to use the blog');
+    }
+    throw err;
   });
+});
+
+router.get('/posts/new', function(req) {
+  return viewEngine.respond('new-post.html', {
+    locals: {
+      title: 'New Post'
+    }
+  });
+});
   
-  post('/posts', function(req) {
-    var post = req.params;
-    post.type = 'post';
+router.post('/posts', function(req) {
+  var post = req.params;
+  post.type = 'post';
+
+  return db.saveDoc(post).then(function(resp) {
+    return bogart.redirect('/posts');
+  });
+});
+
+router.get('/posts/:id', function(req) {
+  return db.openDoc(req.params.id).then(function(post) {
+    return viewEngine.respond('post.html', { locals: post });
+  });
+});
+
+router.post('/posts/:id/comments', function(req) {
+  var comment = req.params;
+
+  return db.openDoc(req.params.id).then(function(post) {
+    post.comments = post.comments || [];
+    post.comments.push(comment);
 
     return db.saveDoc(post).then(function(resp) {
-      return bogart.redirect('/posts');
-    });
-  });
-
-  get('/posts/:id', function(req) {
-    return db.openDoc(req.params.id).then(function(post) {
-      return viewEngine.respond('post.html', { locals: post });
-    });
-  });
-
-  post('/posts/:id/comments', function(req) {
-    var comment = req.params;
-
-    return db.openDoc(req.params.id).then(function(post) {
-      post.comments = post.comments || [];
-      post.comments.push(comment);
-
-      return db.saveDoc(post).then(function(resp) {
-        return bogart.redirect('/posts/'+req.params.id);
-      });
+      return bogart.redirect('/posts/'+req.params.id);
     });
   });
 });
 
-// Add ParseForm middleware to automatically process the parameters
-// of form submissions into a JSON object that is easily usable (req.body, req.params).
-app = bogart.middleware.ParseForm(app);
+var app = bogart.app();
 
-// Start the JSGI server.
-bogart.start(app);
+// Include batteries, a default JSGI stack.
+app.use(bogart.batteries);
+
+// Include our router, it is significant that this is included after batteries.
+app.use(router);
+
+app.start();
