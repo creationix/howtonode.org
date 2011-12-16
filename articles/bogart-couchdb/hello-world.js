8c7a7a1a287c6e8b80dc2e5228d8c67a01f243ae
nrstott <nrstott@gmail.com>
2011-12-16 09:26:08 -0600

    updated hello-world.js bogart example

--- articles/bogart-couchdb/hello-world.js
+++ articles/bogart-couchdb/hello-world.js
@@ -1,9 +1,9 @@
 var bogart = require('bogart');
 
-var app = bogart.router(function(get, post, update, destroy) {
-    get('/', function() {
-        return bogart.html('Hello World');
-    });
+var router = bogart.router();
+
+router.get('/', function() {
+  return bogart.html('Hello World');
 });
 
 bogart.start(app);
