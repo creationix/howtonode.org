f3f5424654d9398531e6e5024ef909f0daebf323
Lenny Witman <dm.sergeenko@gmail.com>
2015-04-17 18:11:54 +0300

    Move code snippets to separate files, fix code formatting

---
+++ articles/deploy-blog-to-heroku/handler.js
@@ -0,0 +1,15 @@
+function serverWorking(response, absPath) {
+  fs.exists(absPath, function(exists) {
+    if (exists) {
+      fs.readFile(absPath, function(err, data) {
+        if (err) {
+          send404(response)
+        } else {
+          sendPage(response, absPath, data);
+        }
+      });
+    } else {
+      send404(response);
+    }
+  });
+}
--- articles/deploy-blog-to-heroku/server.js
+++ articles/deploy-blog-to-heroku/server.js
@@ -1,46 +1,7 @@
 var http = require("http");
-var fs = require("fs");
-var path = require("path");
-var mime = require("mime");
 
-function send404(response) {
-    response.writeHead(404, {"Content-type" : "text/plain"});
-    response.write("Error 404: resource not found");
-    response.end();
-}
-
-function sendPage(response, filePath, fileContents) {
-    response.writeHead(200, {"Content-type" : mime.lookup(path.basename(filePath))});
-    response.end(fileContents);
-}
-
-function serverWorking(response, absPath) {
-    fs.exists(absPath, function(exists) {
-        if (exists) {
-            fs.readFile(absPath, function(err, data) {
-                if (err) {
-                    send404(response)
-                } else {
-                    sendPage(response, absPath, data);
-                }
-            });
-        } else {
-            send404(response);
-        }
-    });
-}
-        
-var server = http.createServer(function(request, response) {
-    var filePath = false;
-
-    if (request.url == "/") {
-        filePath = "public/index.html";
-    } else {
-        filePath = "public" + request.url;
-    }
-
-    var absPath = "./" + filePath;
-    serverWorking(response, absPath);
-});
-
-var port_number = server.listen(process.env.PORT || 3000);
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("It's alive!");
+  response.end();
+}).listen(3000);
